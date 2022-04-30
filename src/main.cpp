7dffe2c72d85caa736d1d86c08a0ca15e7bee117
Marek Pistorius <marek.pistorius@mpistorius.eu>
2022-04-30 12:32:49 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,30 @@
 #include <stdio.h>
 
 
+class Item
+{
+	Item(int x , int y): x(0), y(0) , name(NULL) { }
+	int x;
+	int y;
+	char* name;
+};
+
+
 int main(int argc, char* argv)
 {
+	vector<Item> items;
+	int ret = 0;
+	int in, in2;
+	cout << "Select Target: ";
+	cout << "X";
+	cin >> in2;
+	cout << "y";
+	
+	Item it = new Item(in , in2);
 
-
-	return 0;
+	auto fIt = maps.find(it);
+	if(!fIt.end()) {
+			printf("Target %d destroyed\n", in );	
+	}		
+	return ret;
 }

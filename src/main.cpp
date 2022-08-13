9dd5791298a36eeb4c9c7a5577f8437c6cfead7f
Marek Pistorius <me@maniu.eu>
2022-08-13 14:06:47 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,63 @@
 #include <stdio.h>
+#include <vector>
+#include <string.h>
+#include <iostream>
+#include <algorithm>
+#include <cstdlib>
 
+using namespace std;
 
 class Item
-{
-	Item(int x , int y): x(0), y(0) , name(NULL) { }
-	int x;
-	int y;
-	char* name;
+ {
+ public:
+	int x; /* Point X */
+	int y; /* Point Y */
 };
 
-
-int main(int argc, char* argv)
+vector<Item*> fillItemsWithRandomNumbers(int x)
 {
-	vector<Item> items;
+	vector<Item*> vec;
+	for(int i = 0; i < x;++i)
+	{
+			char * xyz = (char*)malloc(sizeof(char*));
+			int r = rand() % 100 + 1;
+			int r2 = (rand() % 100 + 1 ) / 2;
+			Item *it = new Item();
+			it->x = r;
+			it->y = r2;
+			vec.push_back(it);
+	}
+	return vec;
+}
+
+int main(int argc, char** argv)
+{
+	vector<Item*> items;
+    srand (time(NULL));
+	items = fillItemsWithRandomNumbers(100);
 	int ret = 0;
 	int x, y;
-	cout << "Select Target: ";
-	cout << "X";
+	cout << "Select Target: ( numbers ) " << endl;
+	cout << "X : ";
 	cin >> x;
-	cout << "y";
+	cout << endl << "y :";
 	cin >> y;
-	
-	Item it = new Item(x , y);
+	cout << endl;
 
-	auto fIt = std::find(items.begin(), items.end() , it);
-	if(fIt != items.end() ) {
-			printf("Target %d destroyed\n", in );	
-	}		
+	Item *ItemX = new Item();
+	ItemX->x = x;
+	ItemX->y = y;
+
+	for(auto it = items.begin(); it != items.end(); ++it)
+	{
+			if( ( (*it)->x == x) && ((*it)->y == y)) {
+					cout << "item found" << endl;
+					break;
+			}else{
+					//cout << "not found" << endl;	
+			}
+	}	
+
+	
 	return ret;
 }
